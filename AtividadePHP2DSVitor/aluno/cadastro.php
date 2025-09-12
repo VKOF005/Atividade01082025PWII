f8637e2af7d3984a9ceb4c37bd2de8525f93e041
VKOF005 <vitor.fujihara@etec.sp.gov.br>
2025-09-12 18:18:11 -0300

    Add files via upload

--- AtividadePHP2DSVitor/aluno/cadastro.php
+++ AtividadePHP2DSVitor/aluno/cadastro.php
@@ -12,23 +12,28 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div>
+    <div id = "container">
+        <div id = "form">
 
-        <h1> Cadastro de Aluno </h1>
-        <form class = "txt" action = "processa php" method = "post">
-            <label for="name">Nome</label>
-            <input class = "alL" type = "text" name = "name" id = "name" placeholder = "Nome completo" required> 
+            <h1> Cadastro de Aluno </h1>
+            <form class = "form" action = "processa php" method = "post">
+                <label for="name">Nome</label>
+                <input class = "alL" type = "text" name = "name" id = "name" placeholder = "Nome completo" required> 
 
-            <label for="name">Email</label>
-            <input class = "alL" type = "email" name = "email" id = "email" placeholder = "Informe o email" required>
+                <label for="email">Email</label>
+                <input class = "alL" type = "email" name = "email" id = "email" placeholder = "Informe o email" required>
 
-            <label for="Rm">RM</label>
-            <input class = "alL" type = "int" name = "rm" id = "rm" placeholder = "Informe o RM" required>
+                <label for="Rm">RM</label>
+                <input class = "alL" type = "int" name = "rm" id = "rm" placeholder = "Informe o RM" required>
 
-            <label for="Cpf">CPF</label>
-            <input class = "alL" type = "int" name = "cpf" id = "cpf" placeholder = "Informe o CPF" required> 
+                <label for="Cpf">CPF</label>
+                <input class = "alL" type = "int" name = "cpf" id = "cpf" placeholder = "Informe o CPF" required> 
 
-        </form>
+            </form>
+
+            <button class = "btnCadastrar" >Cadastrar</button>
+        </div>
     </div>
+
 </body>
 </html>
